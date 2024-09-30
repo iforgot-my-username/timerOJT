1cc56e675ec562d3680caec9d04197aa6d1c4104
iforgot-my-username <garibay2018paul@gmail.com>
2024-10-01 07:10:46 +0800

    sample

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
     path: '/gia',
     element: <Gia />,
   },
+  {
+    path: '/goodmorning',
+    element: <Gia />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
